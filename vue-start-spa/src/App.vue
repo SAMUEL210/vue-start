fde5a45ebe1a75241540d3bf8a2b9576b0c1c850
simarone <samuel210@live.fr>
2024-07-08 15:54:43 +0200

    Vue firt Single Page Application of library app done

---
+++ vue-start-spa/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index">
+    </navbar>
+
+    <page-viewer :page="pages[activePage]"></page-viewer>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import PageViewer from './components/PageViewer.vue';
+
+export default {
+    components: {
+        Navbar,
+        PageViewer
+    },
+
+    data() {
+        return ({
+            activePage: 0,
+            pages: [
+                {
+                    link: { text: 'Home', url: 'index.html' },
+                    pageTitle: 'Home Page',
+                    pageDescription: "Ceci est la page d'accueil!"
+                },
+                {
+                    link: { text: 'About', url: 'about.html' },
+                    pageTitle: 'About page',
+                    pageDescription: "Ceci est la page About"
+                },
+                {
+                    link: { text: 'Contact', url: 'contact.html' },
+                    pageTitle: 'Contact page',
+                    pageDescription: "Ceci est la page Contact"
+                },
+                {
+                    link: { text: 'Blog', url: 'blog.html' },
+                    pageTitle: 'Blog page',
+                    pageDescription: "Ceci est la page Blog"
+                }
+            ]
+        })
+    }
+}
+</script>
---
+++ vue-start-spa/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav :class="[`navbar navbar-expand-lg`, `navbar-${theme}`, `bg-${theme}`]">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My vue</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item" v-for="(page, index) in pages" key="index">
+                    <a class="nav-link" :class="{ active: activePage == index }" aria-current="page"
+                        :href="page.link.url" :title="`This page move to the ${page.link.text}`"
+                        @click.prevent="navLinkClick(index)">{{ page.link.text }}</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button class="btn btn-primary" @click.prevent="changeTheme()">Toogle Nav</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: ['pages', 'activePage', 'navLinkClick'],
+    data() {
+        return {
+            theme: 'dark',
+        }
+    },
+    methods: {
+        changeTheme() {
+            let theme = 'light';
+            if (this.theme == 'light') {
+                theme = 'dark';
+            }
+
+            this.theme = theme;
+        }
+    }
+}
+</script>
